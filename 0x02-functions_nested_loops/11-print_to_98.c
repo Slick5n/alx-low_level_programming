2472b68717d0f6db1e0fe68e6b09228ff1791cd3
slick5n <mieleco@yahoo.com>
2022-12-17 22:46:14 +0100

    print numbers

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * print_to_98 - prints numbers from n to 98
+ * @n: starting number
+ */
+void print_to_98(int n)
+{
+	for (int i = n; n <= 98; n++)
+	{
+		_putchar(i'\n');
+	}
+}
