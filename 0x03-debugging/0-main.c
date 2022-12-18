8f8a21101dea325be8bcd084b310f81af4f78ff0
slick5n <mieleco@yahoo.com>
2022-12-18 04:26:01 +0100

    0-main.c, main.h

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -9,11 +9,8 @@ int main(void)
 {
 	int i;
 
-	i = 98;
-	if (positive_or_negative(i) > 0)
-		printf("%d\n is positive", i);
-	else if (positive_or_negative(i) == 0)
-		printf("%d\n is zero", i);
-	else
-		printf("%d\n is negative", i);
+	i = 0;
+	positive_or_negative(i);
+
+	return (0);
 }
