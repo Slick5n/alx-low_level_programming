8ae115cc15e9acf458de49269ac74e89d05a9a67
slick5n <mieleco@yahoo.com>
2022-12-19 23:53:20 +0100

    3-print_numbers.c

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_numbers - prints numbers
+ * followed by new line
+ */
+
+void print_numbers(void)
+{
+	int i;
+
+	for (i = 0; i < 10 ; i++)
+	{
+		_putchar(i + '0');
+	}
+	_putchar('\n');
+}
