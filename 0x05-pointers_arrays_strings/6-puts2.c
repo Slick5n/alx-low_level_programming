16db10349cd9e9ad66dbc563bae40871bc20c6dd
slick5n <mieleco@yahoo.com>
2022-12-19 22:56:00 +0100

    6-puts2.c

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * puts2 - check description
+ * Description: prints every other character
+ * @str: input
+ * Return: 0
+ */
+
+void puts2(char *str)
+{
+	int n = 0;
+
+	while (str[n] != '\0')
+	{
+		if (n % 2 == 0)
+			_putchar(str[n]);
+		n++;
+	}
+	_putchar('\n');
+}
