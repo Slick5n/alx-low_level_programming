b2a76240e45235ef64eeda0c2ba3d21fe8b3c572
slick5n <mieleco@yahoo.com>
2022-12-22 22:11:50 +0100

    5-string_toupper.c

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * string_toupper - check Description
+ * Description: changes all lowercase letters od a string  to uppercase
+ * @str:input
+ * Return: 0
+ */
+
+char *string_toupper(char *str)
+{
+	char *s = str;
+
+	while (*str)
+	{
+		if (*str >= 'a' && *str <= 'z')
+			*str = *str - 32;
+
+		str++;
+	}
+	return (s);
+}
