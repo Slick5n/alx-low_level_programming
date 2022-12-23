297ad9095faa8f899a2204b60201091cdc3b0103
slick5n <mieleco@yahoo.com>
2022-12-23 01:50:59 +0100

    104-print_buffer.c

--- 0x06-pointers_arrays_strings/104-print_buffer.c
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -1,6 +1,5 @@
 #include "main.h"
 #include <stdio.h>
-
 /**
  * isPrintableASCII - determines if n is a printable ASCII char
  * @n: integer
@@ -11,7 +10,6 @@ int isPrintableASCII(int n)
 {
 	return (n >= 32 && n <= 126);
 }
-
 /**
  * printHexes - print hex values for string b in formatted form
  * @b: string to print
@@ -22,7 +20,7 @@ int isPrintableASCII(int n)
 void printHexes(char *b, int start, int end)
 {
 	int i = 0;
-	
+
 	while (i < 10)
 	{
 		if (i < end)
@@ -34,7 +32,6 @@ void printHexes(char *b, int start, int end)
 		i++;
 	}
 }
-
 /**
  * printASCII - print ascii values for string b,
  * formatted to replace nonprintable chars with '.'
@@ -42,6 +39,7 @@ void printHexes(char *b, int start, int end)
  * @start: starting position
  * @end: ending position
  */
+
 void printASCII(char *b, int start, int end)
 {
 	int ch, i = 0;
@@ -49,19 +47,19 @@ void printASCII(char *b, int start, int end)
 	while (i < end)
 	{
 		ch = *(b + i + start);
-
+		
 		if (!isPrintableASCII(ch))
 			ch = 46;
 		printf("%c", ch);
 		i++;
 	}
 }
-
 /**
  * print_buffer - prints a buffer
  * @b: string
  * @size: size of buffer
  */
+
 void print_buffer(char *b, int size)
 {
 	int start, end;
