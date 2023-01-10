c5f22c9d2e2c3f86418175a01e9160bada6faaf7
slick5n <mieleco@yahoo.com>
2023-01-11 00:16:08 +0100

    0x0A-argc_argv

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+
+/**
+* _strdup - check description
+* Description: function that returns a pointer to a
+* newly allocated space in memory which contains a
+* copy of the string given as a parameter
+* @str:string
+* Return: Null if str = Null
+*/
+
+char *_strdup(char *str)
+{
+	int i = 0, len = 0;
+	char *n;
+
+	if (str == NULL)
+	{
+		return (NULL);
+	}
+	for (i = 0; str[i] != '\0'; i++)
+		len++;
+	n = malloc(sizeof(char) * (len + 1));
+
+	if (n == NULL)
+	{
+		return (NULL);
+	}
+	for (i = 0; str[i]; i++)
+	{
+		n[i] = str[i];
+	}
+	n[len] = '\0';
+	return (n);
+}
