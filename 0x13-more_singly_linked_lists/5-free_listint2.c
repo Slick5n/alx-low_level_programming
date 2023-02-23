d0a815ae5230050f95f06f5bb63afceaa21bc443
slick5n <mieleco@yahoo.com>
2023-02-23 15:53:04 -0800

    0x13-more_singly_linked_lists

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,26 @@
+#include "lists.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+* free_listint2 - check description
+* Description: frees a listint_t list
+* @head:pointer
+* Return:void
+*/
+
+void free_listint2(listint_t **head)
+{
+	listint_t *t;
+	
+	if (head == NULL || *head == NULL)
+	{
+		return;
+	}
+	while (*head)
+	{
+		t = *head;
+		*head = (*head)->next;
+		free(t);
+	}
+	*head = NULL;
+}
---
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -0,0 +1,25 @@
+#include "lists.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+* pop_listint - check description
+* Description: deletes the head node of a listint_t
+* @head:pointer
+* Return: 0
+*/
+
+int pop_listint(listint_t **head)
+{
+	int d = 0;
+	listint_t *t;
+
+	if (*head == NULL)
+		return (0);
+
+	d = (*head)->n;
+	t = *head;
+	(*head) = (*head)->next;
+	free(t);
+	return (d);
+}
