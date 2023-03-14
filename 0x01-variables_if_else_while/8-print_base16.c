8f36517a3d591bfaeb7010cfd3da709be65f0443
slick5n <mieleco@yahoo.com>
2023-03-14 10:55:51 +0000

    8-print_base16.c

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -9,12 +9,15 @@
 int main(void)
 {
 	int d;
-	char low;
+	/*char low;*/
 
-	for (d = '0'; d <= '9'; d++)
-	putchar(d);
-	for (low = 'a'; low <= 'f'; low++)
-	putchar(low);
-	putchar('\n');
+	for (d = '0'; d <= '15'; d++)
+	{
+		putchar(d);
+	}
+	/** for (low = 'a'; low <= 'f'; low++)
+	 * putchar(low);
+	 * putchar('\n');
+	 */
 	return (0);
 }
