ea930b0ac808869982735fa1f2b8b20ab351b047
Mike5n <mieleco@yahoo.com>
2024-02-02 22:05:40 +0100

    singly list update

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -18,11 +18,11 @@ list_t *add_node_end(list_t **head, const char *str)
 	
 	if (new_node == NULL)
 		return (NULL);
+
 	new_node->str = strdup(str);
 	new_node->len = strlen(str);
 	new_node->next = NULL;
 	last = *head;
-	
 	if (last == NULL)
 	{
 		*head = new_node;
